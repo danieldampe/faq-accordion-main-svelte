a4500ec1d985ccc1fb33fa6569f40c90728faed6
danieldampe <damper.daniel.sm@gmail.com>
2024-04-18 17:53:19 -0500

    adding attribution

---
+++ src/lib/Attribution.svelte
@@ -0,0 +1,19 @@
+<footer>
+  Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>Frontend Mentor</a>.
+  Coded by <a href='https://github.com/danieldampe' target='_blank'>danieldampe</a>.
+</footer>
+
+<style>
+  footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 8px;
+    font-size: 11px;
+    text-align: center;
+    background-color: transparent;
+  }
+
+  a { color: hsl(228, 45%, 44%); }
+</style>
